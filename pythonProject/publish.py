901bbec5e7640dfc3a464a6c199fce0253e903eb
niko4u <niko4u@fam-email.de>
2020-09-19 15:09:38 +0200

    receive and print text on ESP

--- pythonProject/publish.py
+++ pythonProject/publish.py
@@ -3,5 +3,10 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client()
 client.connect("192.168.178.117", 1883, 60)
 
-client.publish("test/temperatur", "25 C")
+client.publish("inTopic", "1")
+
+for i in range(10):
+    test = input()
+    client.publish("inTopic", test)
+
 client.disconnect()
