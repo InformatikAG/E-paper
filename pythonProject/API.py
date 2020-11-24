dd7d61dcf42c36b1917226004ff559c3430abac4
niko4u <niko4u@fam-email.de>
2020-11-24 20:41:07 +0100

    this script writes the time table of specified rooms into a file as a pure python object using pickle.

--- pythonProject/API.py
+++ pythonProject/API.py
@@ -1,32 +1,107 @@
 import webuntis
 import datetime
+import pickle
+
+requestedRooms = {"2.312", "2.311", "2.310"}
+
+today = datetime.date.today()
+monday = today - datetime.timedelta(days=today.weekday())
+friday = monday + datetime.timedelta(days=4)
+
+
+def convertTeachers(hour):
+    try:
+        new = []
+        for i in hour.teachers:
+            new.append(i.long_name)
+        return new
+    except:
+        return []
+
+
+def convertRooms(hour):
+    try:
+        new = []
+        for i in hour.rooms:
+            new.append(i.long_name)
+        return new
+    except:
+        return []
+
+
+def convertOriginalTeachers(hour):
+    try:
+        new = []
+        for i in hour.original_teachers:
+            new.append(i.long_name)
+        return new
+    except:
+        return []
+
+
+def convertOriginalRooms(hour):
+    try:
+        new = []
+        for i in hour.original_rooms:
+            new.append(i.long_name)
+        return new
+    except:
+        return []
+
+
+def convertSubjects(hour):
+    try:
+        new = []
+        for i in hour.subjects:
+            new.append(i.long_name)
+        return new
+    except:
+        return []
+
+
+def convertKlassen(hour):
+    try:
+        new = []
+        for i in hour.klassen:
+            new.append(i.long_name)
+        return new
+    except:
+        return []
+
+
+def timetableToPy(timetable):
+    table = []
+    for hour in timetable:
+        table.append({
+            "teachers": convertTeachers(hour),
+            "original_teachers": convertOriginalTeachers(hour),
+            "rooms": convertRooms(hour),
+            "original_rooms": convertOriginalRooms(hour),
+            "start": hour.start,
+            "end": hour.end,
+            "subjects": convertSubjects(hour),
+            "klassen": convertKlassen(hour),
+            "code": hour.code
+        })
+    return table
+
 
 with webuntis.Session(
-    username='', # username
-    password='', # password
-    server='tipo.webuntis.com',
-    school='TBZ Mitte Bremen',
-    useragent='E-Paper AG'
+        username='',  # username
+        password='',  # password
+        server='tipo.webuntis.com',
+        school='TBZ Mitte Bremen',
+        useragent='E-Paper AG'
 ).login() as s:
-    today = datetime.date.today()
-    monday = today - datetime.timedelta(days=today.weekday())
-    friday = monday + datetime.timedelta(days=4)
+    with open("test", "wb") as file:
+        data = {}
 
-    print(s.timegrid_units)
+        rooms = s.rooms().filter(name=requestedRooms)
+        print(rooms)
 
-    student = s.get_student(surname="otten",fore_name="niklas")
-    tt = s.timetable(student=student, start=friday, end=friday)
+        for room in rooms:
+            tt = s.timetable(room=room, start=today, end=today)
+            data.update({room.name: timetableToPy(tt)})
 
-    def next_hour_start_time(hours):
-        delta_time = datetime.timedelta(days=-356)
-        for hour in hours:
-            if datetime.timedelta() >= (hour.start-datetime.datetime(year=2020, month=10, day=9, hour=10)) > delta_time:
-                next_start = hour.start
-                delta_time = hour.start-datetime.datetime(year=2020, month=10, day=9, hour=10)
-        return next_start
-
-    print(next_hour_start_time(tt))
-
-    for hour in tt:
-        if hour.start == datetime.datetime(year=2020, month=10, day=9, hour=10):
-            print(hour.subjects)
+        pickle.dump(data, file)
+        print(data)
