774b7e7f06c03fad57d98f773b22b9dc5778db39
Niko4u5 <niko4u@fam-email.de>
2020-11-25 11:26:02 +0100

    added nextTime function

--- pythonProject/ReadFile.py
+++ pythonProject/ReadFile.py
@@ -5,10 +5,19 @@ with open("test","rb") as infile:
     rooms = pickle.load(infile)
 
 
-def findCurentHour(room):
+def curentHour(room):
     for hour in room:
         if (hour["start"] < datetime.datetime.now()) & (hour["end"] > datetime.datetime.now()):
             return hour
 
 
-print(findCurentHour(rooms["2.311"]))
+def nextTime(room):
+    time = datetime.datetime.now() + datetime.timedelta(days=1)
+    for hour in room:
+        if time > hour["start"] > datetime.datetime.now():
+            time = hour["start"]
+        if time > hour["end"] > datetime.datetime.now():
+            time = hour["end"]
+    return time
+
+print(nextTime(rooms["2.312"]))
