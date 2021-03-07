270f59811f69a01033b52fbedf0e9115cdbb89b5
Niko4u5 <niko4u@fam-email.de>
2021-03-07 16:05:33 +0100

    fixed format of FakeAPI file

--- pythonProject/FakeAPI.py
+++ pythonProject/FakeAPI.py
@@ -3,10 +3,10 @@ import pickle
 from math import ceil
 speed = 5  # in minutes
 length = 120  # in minutes
-teachers = ["1", "2", "3", "4"]
-original_teachers = ["4", "1", "2", "3"]
-subjects = ["IFT", "TPR"]
-klassen = ["191", "192", "193"]
+teachers = [["Einz", "1"], ["Zwei", "2"], ["Drei", "3"], ["Vier", "4"]]
+original_teachers = [["Vier", "4"], ["Einz", "1"], ["Zwei", "2"], ["Drei", "3"]]
+subjects = [["Informations Technik", "IFT"], ["Technick Praxis", "TPR"]]
+klassen = [["Informations Technik", "191"], ["Mechatronick", "192"], ["Luft- und Raumfahrttechnick", "193"]]
 
 
 def timetableToPy(room):
@@ -14,14 +14,14 @@ def timetableToPy(room):
     table = []  # creates a list
     for i in range(ceil(120/speed)):  # goes through all hours and appends a dictionary of all Characteristics
         table.append({
-            "teachers": teachers[i % len(teachers)],
-            "original_teachers": original_teachers[i % len(original_teachers)],
-            "rooms": room,
-            "original_rooms": rooms[i % len(rooms)],
+            "teachers": [teachers[i % len(teachers)]],
+            "original_teachers": [original_teachers[i % len(original_teachers)]],
+            "rooms": [room],
+            "original_rooms": [rooms[i % len(rooms)]],
             "start": datetime.datetime.now().replace(minute=0, second=0, microsecond=0) + datetime.timedelta(minutes=speed*i),
             "end": datetime.datetime.now().replace(minute=0, second=0, microsecond=0) + datetime.timedelta(minutes=speed*(i + 1)),
-            "subjects": subjects[i % len(subjects)],
-            "klassen": klassen[i % len(klassen)],
+            "subjects": [subjects[i % len(subjects)]],
+            "klassen": [klassen[i % len(klassen)]],
             # "code": hour.code
         })
     return table
